28deccefc6c036ce7788dd9ceed50e15c21fce7d
Stéphane GUILLY <stephane.guilly@gmail.com>
2020-01-26 18:08:32 +0100

    add pg and apache command in run.sh

--- run.sh
+++ run.sh
@@ -110,5 +110,62 @@ if [ "$1" = "run" ]; then
     exit 0
 fi
 
+if [ "$1" = "pg" ]; then
+    # Clean /tmp
+    rm -rf /tmp/*
+
+    # Fix postgres data privileges
+    chown postgres:postgres /var/lib/postgresql -R
+  
+    # Initialize PostgreSQL
+    createPostgresConfig
+    service postgresql start
+  
+    setPostgresPassword   
+
+    # Run while handling docker stop's SIGTERM
+    stop_handler() {
+        kill -TERM "$child"
+    }
+    trap stop_handler SIGTERM
+
+    service postgresql stop
+
+    exit 0
+fi
+
+if [ "$1" = "apache" ]; then
+    # Clean /tmp
+    rm -rf /tmp/*
+
+    # Configure Apache CORS
+    if [ "$ALLOW_CORS" == "1" ]; then
+        echo "export APACHE_ARGUMENTS='-D ALLOW_CORS'" >> /etc/apache2/envvars
+    fi
+
+    # Initialize Apache
+    service apache2 restart
+
+    # Configure renderd threads
+    sed -i -E "s/num_threads=[0-9]+/num_threads=${THREADS:-4}/g" /usr/local/etc/renderd.conf
+
+    # start cron job to trigger consecutive updates
+    if [ "$UPDATES" = "enabled" ]; then
+      /etc/init.d/cron start
+    fi
+
+    # Run while handling docker stop's SIGTERM
+    stop_handler() {
+        kill -TERM "$child"
+    }
+    trap stop_handler SIGTERM
+
+    sudo -u renderer renderd -f -c /usr/local/etc/renderd.conf &
+    child=$!
+    wait "$child"
+
+    exit 0
+fi
+
 echo "invalid command"
 exit 1
